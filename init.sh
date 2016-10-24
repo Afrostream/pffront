e6c42125b638a84fcf0cc7d3954873a906265038
spebsd <spebsd@gmail.com>
2016-10-24 18:26:08 +0200

    correction, there is no docker branch for billings, all is working correctly with master

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 git clone -b docker https://afrostream-tech:d51e355769ddd0fd212084c35fb99f71ab4146b2@github.com/Afrostream/afrostream-front-end.git src/afrostream-front-end
 git clone -b docker https://afrostream-tech:d51e355769ddd0fd212084c35fb99f71ab4146b2@github.com/Afrostream/afrostream-back-end.git src/afrostream-back-end
 git clone -b docker https://afrostream-tech:d51e355769ddd0fd212084c35fb99f71ab4146b2@github.com/Afrostream/afrostream-api-v1.git src/afrostream-api-v1
-git clone -b docker https://afrostream-tech:d51e355769ddd0fd212084c35fb99f71ab4146b2@github.com/Afrostream/afrostream-billings.git src/afrostream-billings
+git clone https://afrostream-tech:d51e355769ddd0fd212084c35fb99f71ab4146b2@github.com/Afrostream/afrostream-billings.git src/afrostream-billings
 
 docker-compose build
 docker-compose up
