b0a58322ddfffc81d4bca3152d08d37c6da49d5f
nelsounet <nelsounet@afrostream.tv>
2016-10-27 10:57:32 +0200

    path modification

--- afrostream-back-end/entrypoint.sh
+++ afrostream-back-end/entrypoint.sh
@@ -2,9 +2,10 @@
 
 export DATABASE_URL=postgres://postgres:postgres@`host afrostreampostgres | grep "has address" | cut -f4 -d' '`:5432/postgres
 APP_PATH="/usr/src/app"
+NODE_MODULES_PATH="${APP_PATH}/node_modules"
 
 cd "${APP_PATH}"
-if [ ! -d "${APP_PATH}" ]
+if [ ! -d "${NODE_MODULES_PATH}" ]
 then
   echo "npm install all modules..."
   npm install
