5d02531ce4143a4b83967aa520261425411f4b39
nelsounet <nelsounet@afrostream.tv>
2016-10-27 10:40:05 +0200

    path modification

--- afrostream-api-v1/entrypoint.sh
+++ afrostream-api-v1/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 APP_PATH="/usr/src/app"
+NODE_MODULES_PATH="${APP_PATH}/node_modules"
 
 cd "${APP_PATH}"
-if [ ! -d "${APP_PATH}" ]
+if [ ! -d "${NODE_MODULES_PATH}" ]
 then
   echo "npm install all modules..."
   npm install
