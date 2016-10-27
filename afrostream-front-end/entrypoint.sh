6a251d3430f12d4084815fc1fff92bf7cbcd7272
nelsounet <nelsounet@afrostream.tv>
2016-10-27 10:41:40 +0200

    path modification

--- afrostream-front-end/entrypoint.sh
+++ afrostream-front-end/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 APP_PATH="/usr/src/app"
+NODE_MODULES_PATH="${APP_PATH}/node_modules"
 
 echo "upgrading npm version to @3"
 npm install npm@3 -g
 
 cd ${APP_PATH}
-if [ ! -d "${APP_PATH}" ] 
+if [ ! -d "${NODE_MODULES_PATH}" ] 
 then
   echo "FIRST TIME RUN: npm install all modules... This may take a while..."
 
